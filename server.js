8366a12cae55624f9f5bf82b55625080304c86e5
Ajinkya X <ajinkyab1987@gmail.com>
2015-10-31 21:18:14 +0530

    server js

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ var express = require('express'),
     app = express();
 
 app
-    .use(express.static('./public'))
+    .use(express.static(__dirname + '/public'))
     .get('*', function(req, res){
-      res.sendFile('public/main.html')
+      res.sendFile(__dirname + '/public/main.html')
     })
     .listen(3000);
+console.log('running at http://localhost:3000');
